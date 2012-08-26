002036612d9957d855236d4c4559b83a7a027d65
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2012-08-26 22:51:39 +0100

    Added mongo bootstrapping

--- build.cmd
+++ build.cmd
@@ -7,6 +7,16 @@ IF NOT EXIST tools\nuget\NuGet.exe (
   mkdir tools\nuget
   mv -f NuGet.exe tools/nuget
 )
+
+IF NOT EXIST tools\mongo\bin\mongo.exe (
+REM http://www.mongodb.org/dr/downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-v2.0-latest.zip/download
+  wget http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-v2.0-latest.zip
+  unzip mongodb-win32-x86_64-2008plus-v2.0-latest.zip
+  mv mongodb-win32-x86_64-2008plus-v2.0-*/ tools/mongo
+  mkdir -p tools/mongo/data/db
+  rm mongodb-win32-x86_64-2008plus-v2.0-latest.zip
+)
+
 set EnableNuGetPackageRestore=true
 tools\nuget\NuGet.exe install -OutputDirectory libs packages.config
 
