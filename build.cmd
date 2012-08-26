43d80312aa17fa95de726b9ed998ba825e23c586
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2012-08-26 16:41:22 +0100

    Added a build.cmd file that bootstraps the build process by downloading NuGet that should fetch all other library dependencies.

---
+++ build.cmd
@@ -0,0 +1,9 @@
+@echo off
+cls
+
+REM Bootstrap some necessary tools.
+IF NOT EXIST tools\nuget\NuGet.exe (
+  wget http://ci.nuget.org:8080/guestAuth/repository/download/bt4/.lastSuccessful/Console/NuGet.exe
+  mkdir tools\nuget
+  mv -f NuGet.exe tools/nuget
+)
