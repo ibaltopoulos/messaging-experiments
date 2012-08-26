dccdc9c4e813557bd4a42afe0317c911be5eb985
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2012-08-26 18:18:31 +0100

    Added a project file with some funky assembly info code.

---
+++ projects/messaging/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using CommandLine;
+using CommandLine.Text;
+using messaging.Properties;
+
+namespace messaging
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var options = new Options();
+            if (!CommandLineParser.Default.ParseArguments(args, options)) return;
+            
+            // Consume values here
+            if (options.Verbose) Console.WriteLine("Filename: {0}", options.InputFile);
+        }
+    }
+
+    internal sealed class Options : CommandLineOptionsBase
+    {
+        [Option("r", "read", Required = true,
+          HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option("v", "verbose", DefaultValue = true,
+          HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo(AssemblyInfo.Title, AssemblyInfo.Version),
+                Copyright = new CopyrightInfo(AssemblyInfo.Author, 2012),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+            HandleParsingErrorsInHelp(help);
+            help.AddPreOptionsLine("<<license details here.>>");
+            help.AddPreOptionsLine("Usage: CSharpTemplate -tSomeText --numeric 2012 -b");
+            help.AddOptions(this);
+
+            return help;
+        }
+
+        void HandleParsingErrorsInHelp(HelpText help)
+        {
+            if (!LastPostParsingState.Errors.Any()) return;
+
+            var errors = help.RenderParsingErrorsText(this, 2); // indent with two spaces
+            if (string.IsNullOrEmpty(errors)) return;
+
+            help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR(S):"));
+            help.AddPreOptionsLine(errors);
+        }
+    }
+}
---
+++ projects/messaging/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("messaging")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("messaging")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6f91b41a-729a-4c3f-a418-bdfe3f37c59f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace messaging.Properties {
+    public static class AssemblyInfo {
+        public static string Author = "Ioannis Baltopoulos";
+        public static string Email = "ioannis.baltopoulos@gmail.com";
+        public static string Title { get { return Get<AssemblyTitleAttribute>(at => at.Title); } }
+        public static string Company { get { return Get<AssemblyCompanyAttribute>(at => at.Company); } }
+        public static string Copyright { get { return Get<AssemblyCopyrightAttribute>(at => at.Copyright); } }
+        public static string Version { get { return Get<AssemblyVersionAttribute>(at => at.Version); } }
+
+        private static string Get<T>(Func<T, string> getProperty) where T : Attribute {
+            var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(T), true);
+            if (attributes.Length <= 0) return string.Empty;
+            var attr = (T) attributes.First();
+            return getProperty(attr);
+        }
+    }
+}
